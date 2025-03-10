e4aacc74a30c4772a292c11171029e6f35ccd967
xPunio <akcentt72@gmail.com>
2025-03-11 00:19:15 +0100

    1

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+console.log(textOut);
