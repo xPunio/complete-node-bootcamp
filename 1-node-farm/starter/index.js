567cbad8f3ded282c78e5aa16025638d66d737f2
xPunio <akcentt72@gmail.com>
2025-03-11 20:13:37 +0100

    2

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,23 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// bloacking, synchrinouse way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+//
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// console.log(textOut);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-console.log(textOut);
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  if (err) return console.log(err);
+  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", () => {
+        console.log("your file has been written");
+      });
+    });
+  });
+});
+console.log("Will read file");
